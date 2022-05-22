f8173d8de66a1ae3f6c88e7d46a9c9e466ec8943
Roni Torres <ronitls1989@gmail.com>
2022-05-22 01:28:10 -0300

    Criados Controller Cliente e Modelo Cliente

--- Ecommerce/Controllers/ClienteController.cs
+++ Ecommerce/Controllers/ClienteController.cs
@@ -1,5 +1,5 @@
-﻿using Ecommerce.Modelos;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Ecommerce.Modelos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,22 +8,19 @@ using System.Net.Http;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Authorization;
 
-
 namespace Ecommerce.Controllers
 {
-
     [Route("api/[controller]")]
     [ApiController]
-    public class ProdutoController : ControllerBase
-    {
 
+    public class ClienteController : Controller
+    {
         [HttpGet]
-        [Route("CarregaProduto")]
+        [Route("CarregaDepartamento")]
         [AllowAnonymous]
 
-        public async Task<IActionResult> CarregaProduto()
+        public async Task<IActionResult> CarregaCliente()
         {
-
             var responseFull = "";
 
             var baseAddress = new Uri("https://private-anon-b98488d6a3-maximatech.apiary-mock.com/");
@@ -40,14 +37,15 @@ namespace Ecommerce.Controllers
                 }
             }
 
-            var produtoModelo = new List<ProdutoModelo>();
 
-            produtoModelo = JsonConvert.DeserializeObject<List<ProdutoModelo>>(responseFull);
+                var clienteModelo = new List<ClienteModelo>();
+
+                clienteModelo = JsonConvert.DeserializeObject<List<ClienteModelo>>(responseFull);
 
 
-            return Ok(produtoModelo);
+                 return Ok(clienteModelo);
 
-           
+            
         }
     }
 }
---
+++ Ecommerce/Modelos/ClienteModelo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Modelos
+{
+    public class ClienteModelo
+    {
+        public string Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+}
