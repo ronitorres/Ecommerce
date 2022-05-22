07bd6d5faa8eb813110aa4b92e417ad7b6b7e3f3
Roni Torres <ronitls1989@gmail.com>
2022-05-22 01:16:58 -0300

    Criados Controller Produto e Modelo Produto

---
+++ Ecommerce/Controllers/ProdutoController.cs
@@ -0,0 +1,54 @@
+﻿using Ecommerce.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Authorization;
+
+
+
+namespace Ecommerce.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("CarregaProduto")]
+        [AllowAnonymous]
+
+        public async Task<IActionResult> CarregaProduto()
+        {
+
+            var responseFull = "";
+
+            var baseAddress = new Uri("https://private-anon-b98488d6a3-maximatech.apiary-mock.com/");
+
+            using (var httpClient = new HttpClient { BaseAddress = baseAddress })
+            {
+
+                using (var response = await httpClient.GetAsync("fullstack"))
+                {
+
+                    string responseData = await response.Content.ReadAsStringAsync();
+
+                    responseFull = responseData;
+                }
+            }
+
+            var produtoModelo = new List<ProdutoModelo>();
+
+            produtoModelo = JsonConvert.DeserializeObject<List<ProdutoModelo>>(responseFull);
+
+
+            return Ok(produtoModelo);
+
+           
+        }
+    }
+}
---
+++ Ecommerce/Modelos/ProdutoModelo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Modelos
+{
+    public class Produto
+    {
+
+        public string Id { get; set; }
+        public string Codigo { get; set; }
+        public string  Nome { get; set; }
+        public double  PrecoUnitario { get; set; }
+        public string IMG { get; set; }
+
+    }
+}
