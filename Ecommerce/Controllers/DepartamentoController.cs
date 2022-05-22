407bea3d50f306de67480fe66aa87519407ea504
Roni Torres <ronitls1989@gmail.com>
2022-05-22 00:58:09 -0300

    Projeto criado

---
+++ Ecommerce/Controllers/DepartamentoController.cs
@@ -0,0 +1,47 @@
+﻿using Ecommerce.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Ecommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartamentoController : ControllerBase
+    {
+        [HttpGet]
+        [Route("CarregaDepartamento")]
+        [AllowAnonymous]
+        public async Task<IActionResult> CarregaDepartamento()
+        {
+            var responseFull = "";
+         
+            var baseAddress = new Uri("https://private-anon-3acfa3e1bd-maximatech.apiary-mock.com/");
+
+            using (var httpClient = new HttpClient { BaseAddress = baseAddress })
+            {
+
+                using (var response = await httpClient.GetAsync("/fullstack/departamento"))
+                {
+
+                    string responseData = await response.Content.ReadAsStringAsync();
+
+                    responseFull = responseData;
+                }
+            }
+
+            var departamentoModelo = new List<DepartamentoModelo>();
+            
+            departamentoModelo = JsonConvert.DeserializeObject<List<DepartamentoModelo>>(responseFull);
+
+
+            return Ok(departamentoModelo);
+        }
+    }
+}
---
+++ Ecommerce/Modelos/DepartamentoModelo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Modelos
+{
+    public class DepartamentoModelo
+    {
+        public string Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+}
